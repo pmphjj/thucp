89a8290b1194f238812b4785c708cf3872bb5b35
pmphjj <hjjgy365@163.com>
2017-10-25 20:57:58 +0800

    读取CP的json文件到内存

--- src/model/CPMRM/StandardCPStage.java
+++ src/model/CPMRM/StandardCPStage.java
@@ -3,10 +3,20 @@ package model.CPMRM;
 import java.util.ArrayList;
 
 public class StandardCPStage {
-	String name; //阶段时间
-	ArrayList<String> coreActivities;//主要诊疗活动
-	ArrayList<String> coreOrdersLong;//长期医嘱
-	ArrayList<String> coreOrdersCur;//临时医嘱
-	ArrayList<String> coreServices;//主要护理
+	public String name; //阶段时间
+	public ArrayList<String> coreActivities;//主要诊疗活动
+	public ArrayList<String> coreOrdersLong;//长期医嘱
+	public ArrayList<String> coreOrdersCur;//临时医嘱
+	public ArrayList<String> coreOrdersOut;//出院医嘱
+	public ArrayList<String> coreServices;//主要护理
+
+	public StandardCPStage(){
+		name = new String();
+		coreActivities = new ArrayList<String>();
+		coreOrdersLong = new ArrayList<String>();
+		coreOrdersCur = new ArrayList<String>();
+		coreOrdersOut = new ArrayList<String>();
+		coreServices = new ArrayList<String>();
+	}
 
 }
